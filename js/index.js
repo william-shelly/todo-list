218bce44e02a9c8fd57a05b4af4c2bb591d1429a
william-shelly <william-shelly@users.noreply.github.com>
2021-01-12 22:01:46 -0600

    Made list pull from storage and list on page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
-console.log('hello');
+let toDoListContainer, myListArray, myList;
+
+toDoListContainer = document.querySelector('#toDoList');
+
+myListArray = ['Fix Fence','Wash Car','Clean Interior','Get Groceries','Fly Kite'];
+
+// myListArray = null;
+
+localStorage.setItem('MyList', JSON.stringify(myListArray));
+
+myList = JSON.parse(localStorage.getItem('MyList'));
+
+if (myList === undefined || myList === null ) {
+    console.log('empty');
+} else {
+    for (let i = 0; i < myList.length; i++ ) {
+        // console.log(myList[i]);
+        buildListItems(i);
+    }
+}
+
+/* toDoListContainer = ''; */
+/* console.log(myList); */
+
+function buildListItems(i) {
+    let eLabel, eInput, listItem;
+    // create label
+    eLabel = document.createElement('label');
+    // label .list-group-item
+    eLabel.className = 'list-group-item';
+    // create input
+    eInput = document.createElement('input');
+    // input .form-check-input .me-0
+    eInput.className = 'form-check-input me-2';
+    // input [type='checkbox'] [value='']
+    eInput.setAttribute("type", "checkbox");
+    eInput.setAttribute("value", myList[i]);
+    // item coffee
+    listItem = document.createTextNode(myList[i]);
+    eLabel.appendChild(eInput);
+    eLabel.appendChild(listItem);
+
+    toDoListContainer.appendChild(eLabel, toDoListContainer);
+}
